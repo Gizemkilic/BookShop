c195470b4d83e6eb8a7ab385a6f1c34bc28deb50
gizem kılıç <gzmkelece@gmail.com>
2019-06-20 13:50:16 +0300

    BookShop Project Created

---
+++ src/BookShop.Core/Book/BookListItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BookShop.Core.Book
+{
+    public class BookListItem
+    {
+        private static int bookListId;
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(300)]
+        [Required]
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;  
+
+        [ForeignKey("BookListId")]
+        public virtual BookList BookList { get; set; }
+        public virtual int BookListId { get; set; }
+
+        [Required]
+        public string CreatorUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public static BookListItem Create(string name, string description, int bookrListId, string creatorUserId)
+        {
+            return new BookListItem
+            {
+                Name = name,
+                BookListId = bookListId,
+                CreatorUserId = creatorUserId,
+                CreatedDate = DateTime.Now,
+                Description = description,
+                IsCompleted = false
+            };
+
+            
+        }
+
+    }
+}
---
+++ src/BookShop.EntityFramework/Contexts/ApplicationUserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookShop.Core.Book;
+using BookShop.Core.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.EntityFramework.Contexts
+{
+    public class ApplicationUserDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationUserDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+             public DbSet<BookList> BookLists { get; set; }
+             public DbSet<BookListItem> BookListItems { get; set; }
+             public DbSet<Books> Books { get; set; }
+
+    }
+}
